28adf93a1dd6e4dbcfac78e416aff6982fd6fe2b
van-kevin suy <vankevinsuy@gmail.com>
2024-04-14 19:45:44 -0400

    build plex with official client arm64

---
+++ backup_data.sh
@@ -0,0 +1,4 @@
+sudo rsync --progress -a /mnt/hdd2T/plex/animated_movies/ /mnt/toshiba/animated_movies
+sudo rsync --progress -a /mnt/hdd2T/plex/movies/ /mnt/toshiba/movies
+sudo rsync --progress -a /mnt/hdd2T/plex/series/ /mnt/toshiba/series
+sudo rsync --progress -a /mnt/hdd2T/plex/photos/ /mnt/toshiba/photos
